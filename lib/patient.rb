d79decc88a51f8a240367712b3ac1499835259f0
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-17 05:48:13 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,3 +2,19 @@ require_relative "./appointment.rb"
 require_relative "./doctor.rb"
 
 class Patient 
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name - name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end  
+  
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end 
+    
